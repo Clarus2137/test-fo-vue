175f1c50ba14b4d4d320777c215d226122ba0af0
Aleksandr Kudriakov <102177353+Clarus2137@users.noreply.github.com>
2022-06-19 17:23:15 +0200

    create posts.js

---
+++ posts.js
@@ -0,0 +1,12 @@
+[
+  {id: 4, title: 'Post-4', body: 'Short description of Post-4'},
+  {id: 5, title: 'Post-5', body: 'Short description of Post-5'},
+  {id: 6, title: 'Post-6', body: 'Short description of Post-6'},
+  {id: 7, title: 'Post-7', body: 'Short description of Post-7'},
+  {id: 8, title: 'Post-8', body: 'Short description of Post-8'},
+  {id: 9, title: 'Post-9', body: 'Short description of Post-9'},
+  {id: 10, title: 'Post-10', body: 'Short description of Post-10'},
+  {id: 11, title: 'Post-11', body: 'Short description of Post-11'},
+  {id: 12, title: 'Post-12', body: 'Short description of Post-12'},
+  {id: 13, title: 'Post-13', body: 'Short description of Post-13'}
+]
